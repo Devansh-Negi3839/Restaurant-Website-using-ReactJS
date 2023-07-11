745179ab16a3f1d0f4f1fe855e9baf00ab563a8f
Devansh Negi <devansh3839@gmail.com>
2023-07-11 18:28:01 +0530

    added Carousel in Menu.jsx

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,35 +1,33 @@
 import React from 'react';
 import Layout from '../components/Layout/Layout';
-import { Box, Card, CardActionArea, CardContent, CardMedia, Typography } from '@mui/material';
-
+import { Card, CardActionArea, CardContent, CardMedia, Typography } from '@mui/material';
+import Carousel from 'react-material-ui-carousel';
 import { MenuList } from '../data/data';
 
 const Menu = () => {
   return (
     <Layout>
-      <Box sx={{ display: 'flex', flexWrap: 'wrap' , justifyContent:'center'}}>
+      
+      <Carousel sx={{margin:'20px 0'}}>
         {MenuList.map((menu) => (
-          <Card key={menu.id}
-            sx={{ maxWidth: '390px', display: 'flex', m: 2 }}>
+          <Card key={menu.id} sx={{ maxWidth: '400px',minHeight:'500px', margin: '10px auto' }}>
             <CardActionArea>
               <CardMedia
-                sx={{ minHeight: '400px' }}
-                component={'img'}
-                src={menu.image}
+                component="img"
+                height="300"
+                image={menu.image}
                 alt={menu.name}
               />
               <CardContent>
-                <Typography variant='h5' gutterBottom component={'div'}>
+                <Typography variant="h5" gutterBottom component="div">
                   {menu.name}
                 </Typography>
-                <Typography variant='body2'>
-                  {menu.description}
-                </Typography>
+                <Typography variant="body2">{menu.description}</Typography>
               </CardContent>
             </CardActionArea>
           </Card>
         ))}
-      </Box>
+      </Carousel>
     </Layout>
   );
 };
